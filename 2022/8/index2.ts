178764f3b17ec8fafec04b92baf4ac7916598621
Kristjan Jonsson <kris@anglingiq.com>
2022-12-10 21:51:02 +0000

    day 8 part2

---
+++ 2022/8/index2.ts
@@ -0,0 +1,227 @@
+import { dir } from "console";
+import * as fs from "fs";
+
+interface TreeInfo {
+  w: number;
+  h: number;
+  value: number;
+}
+
+const getWidthHeight = (grid: number[][]): [number, number] => {
+  return [grid[0].length, grid.length];
+};
+
+const processGrid = (rows: string[]): number[][] => {
+  let result = [];
+
+  rows.forEach((row) => {
+    result.push(row.split(""));
+  });
+
+  return result;
+};
+
+const getKey = (h: number, w: number): string => {
+  return `${h},${w}`;
+};
+
+const updateTree = (
+  distance: number,
+  totalPerTree: number[][],
+  h: number,
+  w: number
+): void => {
+  if (!totalPerTree[h][w]) {
+    totalPerTree[h][w] = distance;
+    return;
+  }
+
+  totalPerTree[h][w] *= distance;
+};
+
+const processFromLeft = (grid: number[][], totalPerTree: number[][]): void => {
+  const [width, height] = getWidthHeight(grid);
+
+  for (let h = 0; h < height; h++) {
+    const sizeStack: TreeInfo[] = [];
+
+    for (let w = width - 1; w >= 0; w--) {
+      const value = grid[h][w];
+
+      while (sizeStack.length > 0) {
+        if (value < sizeStack.at(-1).value) {
+          break;
+        }
+
+        const poppedValue = sizeStack.pop();
+        updateTree(
+          Math.abs(poppedValue.w - w),
+          totalPerTree,
+          poppedValue.h,
+          poppedValue.w
+        );
+      }
+
+      sizeStack.push({ h, w, value });
+    }
+
+    // Need to empty the stack, calculate distances and add to totalPerTree
+    while (sizeStack.length > 0) {
+      const poppedValue = sizeStack.pop();
+      updateTree(
+        Math.abs(poppedValue.w),
+        totalPerTree,
+        poppedValue.h,
+        poppedValue.w
+      );
+    }
+  }
+};
+
+const processFromRight = (grid: number[][], totalPerTree: number[][]): void => {
+  const [width, height] = getWidthHeight(grid);
+
+  for (let h = 0; h < height; h++) {
+    const sizeStack: TreeInfo[] = [];
+
+    for (let w = 0; w < width; w++) {
+      const value = grid[h][w];
+
+      while (sizeStack.length > 0) {
+        if (value < sizeStack.at(-1).value) {
+          break;
+        }
+
+        const poppedValue = sizeStack.pop();
+        updateTree(
+          Math.abs(poppedValue.w - w),
+          totalPerTree,
+          poppedValue.h,
+          poppedValue.w
+        );
+      }
+
+      sizeStack.push({ h, w, value });
+    }
+
+    // Need to empty the stack, calculate distances and add to totalPerTree
+    while (sizeStack.length > 0) {
+      const poppedValue = sizeStack.pop();
+      updateTree(
+        Math.abs(width - 1 - poppedValue.w),
+        totalPerTree,
+        poppedValue.h,
+        poppedValue.w
+      );
+    }
+  }
+};
+
+const processFromTop = (grid: number[][], totalPerTree: number[][]): void => {
+  const [width, height] = getWidthHeight(grid);
+
+  for (let w = 0; w < width; w++) {
+    const sizeStack: TreeInfo[] = [];
+
+    for (let h = height - 1; h >= 0; h--) {
+      const value = grid[h][w];
+
+      while (sizeStack.length > 0) {
+        if (value < sizeStack.at(-1).value) {
+          break;
+        }
+
+        const poppedValue = sizeStack.pop();
+        updateTree(
+          Math.abs(poppedValue.h - h),
+          totalPerTree,
+          poppedValue.h,
+          poppedValue.w
+        );
+      }
+
+      sizeStack.push({ h, w, value });
+    }
+
+    // Need to empty the stack, calculate distances and add to totalPerTree
+    while (sizeStack.length > 0) {
+      const poppedValue = sizeStack.pop();
+      updateTree(
+        Math.abs(poppedValue.h),
+        totalPerTree,
+        poppedValue.h,
+        poppedValue.w
+      );
+    }
+  }
+};
+
+const processFromBottom = (
+  grid: number[][],
+  totalPerTree: number[][]
+): void => {
+  const [width, height] = getWidthHeight(grid);
+
+  for (let w = 0; w < width; w++) {
+    const sizeStack: TreeInfo[] = [];
+
+    for (let h = 0; h < height; h++) {
+      const value = grid[h][w];
+
+      while (sizeStack.length > 0) {
+        if (value < sizeStack.at(-1).value) {
+          break;
+        }
+
+        const poppedValue = sizeStack.pop();
+        updateTree(
+          Math.abs(poppedValue.h - h),
+          totalPerTree,
+          poppedValue.h,
+          poppedValue.w
+        );
+      }
+
+      sizeStack.push({ h, w, value });
+    }
+
+    // Need to empty the stack, calculate distances and add to totalPerTree
+    while (sizeStack.length > 0) {
+      const poppedValue = sizeStack.pop();
+      updateTree(
+        Math.abs(height - 1 - poppedValue.h),
+        totalPerTree,
+        poppedValue.h,
+        poppedValue.w
+      );
+    }
+  }
+};
+
+const firstPart = () => {
+  const fileData = fs.readFileSync(__dirname + "/input.txt", "utf-8");
+  const rows = fileData.split("\n");
+
+  const grid = processGrid(rows);
+  const [width, height] = getWidthHeight(grid);
+
+  // Create grid of totals per tree
+  let totalPerTree = new Array(height);
+  for (var i = 0; i < totalPerTree.length; i++) {
+    totalPerTree[i] = new Array(width).fill(0);
+  }
+
+  // console.log(grid);
+  processFromLeft(grid, totalPerTree);
+  // console.log(totalPerTree);
+  processFromRight(grid, totalPerTree);
+  // console.log(totalPerTree);
+  processFromTop(grid, totalPerTree);
+  // console.log(totalPerTree);
+  processFromBottom(grid, totalPerTree);
+  // console.log(totalPerTree);
+
+  console.log("answer is", Math.max.apply(null, totalPerTree.flat()));
+};
+
+firstPart();
